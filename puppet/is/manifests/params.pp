6c65721248e5d1a03197c3268a601e44b2298c9d
ctienshi <ctienshi@gmail.com>
2019-05-07 10:45:03 +0530

    Add a common Puppet module

--- puppet/is/manifests/params.pp
+++ puppet/is/manifests/params.pp
@@ -36,6 +36,7 @@ class is570::params {
   $http_proxy_port  = '80'
   $https_proxy_port = '443'
   $is_package = 'wso2is-5.7.0.zip'
+  $is_analytics = 'CF_ANALYTICS_IP'
 
   # Define the templates
   $start_script_template = 'bin/wso2server.sh'
@@ -50,6 +51,11 @@ class is570::params {
     'repository/conf/registry.xml',
     'repository/conf/tomcat/catalina-server.xml',
     'repository/conf/consent-mgt-config.xml',
+    'repository/deployment/server/eventpublishers/IsAnalytics-Publisher-wso2event-AuthenticationData.xml',
+    'repository/deployment/server/eventpublishers/IsAnalytics-Publisher-wso2event-RoleData.xml',
+    'repository/deployment/server/eventpublishers/IsAnalytics-Publisher-wso2event-SessionData.xml',
+    'repository/deployment/server/eventpublishers/IsAnalytics-Publisher-wso2event-UserData.xml',
+#    'repository/conf/log4j.properties',
   ]
 
   $clustering               = {
@@ -63,6 +69,9 @@ class is570::params {
   } elsif $jdk_version == 'OPEN_JDK8' {
     $jdk_type = "jdk-8u192-ea-bin-b02-linux-x64-19_jul_2018.tar.gz"
     $jdk_path = "jdk1.8.0_192"
+  } elsif $jdk_version == 'Corretto_JDK8' {
+    $jdk_type = "amazon-corretto-8.202.08.2-linux-x64.tar.gz"
+    $jdk_path = "amazon-corretto-8.202.08.2-linux-x64"
   }
 
   # ----- Master-datasources config params -----
@@ -81,7 +90,7 @@ class is570::params {
     $db_driver_class_name = 'com.mysql.jdbc.Driver'
     $db_connector = 'mysql-connector-java-5.1.41-bin.jar'
     $db_validation_query = 'SELECT 1'
-  } elsif $db_managment_system == 'oracle-se2' {
+  } elsif $db_managment_system =~ 'oracle' {
     $wso2_reg_db_name = 'WSO2IS_REG_DB'
     $wso2_bps_db_name = 'WSO2IS_BPS_DB'
     $wso2_user_db_name = 'WSO2IS_USER_DB'
@@ -160,7 +169,7 @@ class is570::params {
 
   $wso2_consent_db = {
     url               => $wso2_consent_db_url,
-    username          => $wso2wso2_consent_db_name,
+    username          => $wso2_consent_db_name,
     password          => $db_password,
     driver_class_name => $db_driver_class_name,
     validation_query  => $db_validation_query,
