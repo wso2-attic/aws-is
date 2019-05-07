738f9844adedf52bc5a9272c15a720650bbbb4ca
ctienshi <ctienshi@gmail.com>
2019-05-07 16:52:33 +0530

    Add Puppet modules for IS Analytics

---
+++ puppet/is_analytics_dashboard570/manifests/startserver.pp
@@ -0,0 +1,26 @@
+# ----------------------------------------------------------------------------
+#  Copyright (c) 2018 WSO2, Inc. http://www.wso2.org
+#
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+# ----------------------------------------------------------------------------
+
+# Class is_analytics_dashboard580::startserver
+# Starts the server as a service in the final stage.
+class is_analytics_dashboard580::startserver inherits is_analytics_dashboard580::params {
+
+  # Start the service
+  # service { $service_name:
+  #   ensure => running,
+  #   enable => true,
+  # }
+}
